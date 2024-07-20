761ae9800784b2f149cbdfe708b568a91225cb49
ZHE-WEN, DU <duzhewen0404@gmail.com>
2024-07-20 22:06:18 +0800

    Update fire_detect.py

--- fire_detect.py
+++ fire_detect.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2
 
-# 初始化背景减法器和结构元素
+# 初始化背景減法器和結構元素
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
 
-# 定义火焰颜色范围
+# 定義火焰顏色範圍
 lower_fire = np.array([18, 50, 50], dtype='uint8')
 upper_fire = np.array([35, 255, 255], dtype='uint8')
 
@@ -13,7 +13,7 @@ upper_fire = np.array([35, 255, 255], dtype='uint8')
 upper_smoke = [124, 180, 180]'''
 
 def detect_fire(frame):
-    # 转换为 HSV 颜色空间并模糊处理
+    # 轉換為 HSV 色彩空間並模糊處理
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     blur = cv2.GaussianBlur(hsv, (21, 21), 0)
     
@@ -21,7 +21,7 @@ def detect_fire(frame):
     upper_smoke_array = np.array(upper_smoke, dtype='uint8')
     smoke_mask = cv2.inRange(hsv, lower_smoke_array, upper_smoke_array)'''
     
-    # 火焰颜色掩码
+    # 火焰顏色掩碼
     fire_mask = cv2.inRange(blur, lower_fire, upper_fire)
     
     # 背景减法
@@ -29,7 +29,7 @@ def detect_fire(frame):
     gray_blur = cv2.GaussianBlur(gray, (5, 5), 0)
     fmask = fgbg.apply(gray_blur)
     
-    # 形态学操作
+    # 形態學操作
     fmask = cv2.medianBlur(fmask, 5)
     fmask = cv2.erode(fmask, kernel, iterations=2)
     fmask = cv2.dilate(fmask, kernel, iterations=2)
@@ -37,11 +37,11 @@ def detect_fire(frame):
     # 结合火焰掩码和背景掩码
     combined_mask = cv2.bitwise_and(fmask, fire_mask)
     
-    # 轮廓检测
+    # 輪廓檢測
     contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 500:  # 面积阈值
+        if area > 500:  # 面積阈值
             x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
